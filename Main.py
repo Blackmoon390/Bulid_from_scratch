55f291b7fc1d31abdbb76469a18aad369f2341bd
Blackmoon390 <senseicoder09@gmail.com>
2025-12-18 17:21:44 +0530

    buildingbase

--- Main.py
+++ Main.py
@@ -1,4 +1,14 @@
 import numpy as np #just used numpy for exponential function and max
 
 class nn():
-    
+    def __init__(self,input_layer,hidden_layer,output_layer,lr=0.001):
+        np.random.seed(42)
+        self.input_layer=input_layer
+        self.hidden_layer=hidden_layer
+        self.output_layer=output_layer
+        self.lr=lr
+
+        self.w1=np.random.randn(self.input_layer,self.output_layer)*0.1
+        self.b1=np.zeros(1,self.hidden_layer)
+        
+        
