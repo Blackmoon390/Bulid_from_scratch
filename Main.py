ca5ae88c57e85d7e67edec46366359b2b8d36e3a
Blackmoon390 <senseicoder09@gmail.com>
2025-12-19 16:09:27 +0530

    update

--- Main.py
+++ Main.py
@@ -20,7 +20,7 @@ class nn():
         self.b3=np.zeros((1,self.output_layer))
 
     def relu(self,z):
-        return np.max(z,0)
+        return np.maximum(z,0)
     
     def relu_derivative(self,z):
         return (z > 0).astype(float)
@@ -31,11 +31,11 @@ class nn():
     def forward(self,x):
         self.z1=x@self.w1+self.b1
         self.a1=self.relu(self.z1)
-        print(self.a1)
+        # print(self.a1)
 
         self.z2=self.a1@self.w2+self.b2
         self.a2=self.relu(self.z2)
-        print(self.a2)
+        # print(self.a2)
 
         self.z3=self.a2@self.w3+self.b3
         self.y_pred=self.sigmoid(self.z3)
@@ -51,15 +51,57 @@ class nn():
         db3=np.mean(dz3,axis=0,keepdims=True)
 
 
-        da1=dz3 @ self.w3.T
-        dz1=da1*self.relu_derivative(self.z2)
-        dw2=self.a1.T@dz1/m
-        db2=np.mean(dz1,axis=0,keepdims=True)
+        da2=dz3 @ self.w3.T
+        dz2=da2*self.relu_derivative(self.z2)
+        dw2=self.a1.T @dz2/m
+        db2=np.mean(dz2,axis=0,keepdims=True)
+
+        da1=dz2 @ self.w2.T
+        dz1=da1*self.relu_derivative(self.z1)
+        dw1=x.T@dz1/m
+        db1=np.mean(dz1,keepdims=True,axis=0)
+
+        self.w1-=self.lr*dw1
+        self.w2-=self.lr*dw2
+        self.w3-=self.lr*dw3
+
+        self.b1-=self.lr*db1
+        self.b2-=self.lr*db2
+        self.b3-=self.lr*db3
+    def loss(self, y):
+        eps = 1e-9
+        return -np.mean(y*np.log(self.y_pred+eps) +
+                        (1-y)*np.log(1-self.y_pred+eps))
+        
 
 
  
-hello=nn(2,3,1)
-array=np.array([[23,5]])
+data=pd.read_csv("dataset2.csv")
 
-print(hello.forward(array))
+x=data.drop(columns=["motor"]).to_numpy()
+y=data["motor"].to_numpy().reshape(-1,1)
+
+print(x.shape,y.shape)
+
+
+network=nn(9,16,1,0.001)
+
+for epoch in range(0,2000):
+    network.forward(x)
+    los=network.loss(y)
+    if epoch%100 ==0:print(los)
+    network.backpropagation(x,y)
+
+
+
+
+
+
+k=[12,0,39,80,24,0,2,3,2]
+
+print(network.forward(k))
+
+
+
+
         
