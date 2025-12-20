17330997ffc43d7b734abdd2c425b90350229204
Blackmoon390 <senseicoder09@gmail.com>
2025-12-20 13:02:56 +0530

    reback same nn with high epoch got better accuracy than single wieght perceptron

--- Main.py
+++ Main.py
@@ -1,61 +1,77 @@
 import numpy as np #just used numpy for mean,random,array
 import pandas as pd # for split  training set ,test set
 
-class single_weighted_perceptron():
-    def __init__(self,lr=0.01,epoch=1000000):
-
-        self.lr=lr
-        self.epoch=epoch
-
-    def fit(self,x,y):
-
-        m,n=x.shape
-
+class nn():
+    def __init__(self,input_layer,hidden_layer,output_layer,lr=0.001):
         np.random.seed(42)
-        self.w=np.random.randn(n,1)*0.01
+        self.input_layer=input_layer
+        self.hidden_layer=hidden_layer
+        self.hidden_layer2=8
+        self.output_layer=output_layer
+        self.lr=lr
 
-        pos=np.sum(y==1)
-        neg=np.sum(y==0)
+        self.w1=np.random.randn(self.input_layer,self.hidden_layer)*0.1
+        self.b1=np.zeros((1,self.hidden_layer))
 
-        self.w1=m/(pos*2)
-        self.w0=m/(neg*2)
-        self.b=0.0
+        self.w2=np.random.randn(self.hidden_layer,self.hidden_layer2)*0.1
+        self.b2=np.zeros((1,self.hidden_layer2))
 
-        itergap=self.epoch/10
+        self.w3=np.random.randn(self.hidden_layer2,self.output_layer)*0.1
+        self.b3=np.zeros((1,self.output_layer))
 
-        for _ in range(self.epoch):
-            z=x@self.w+self.b
-            ycap=self.sigmoid(z)
-
-            dz = (self.w1 * y + self.w0 * (1 - y)) * (ycap - y)
-            dw=x.T@dz/m
-            db=np.mean(dz)
-
-            if (_ % itergap == 0):
-                 lossval = self.loss(ycap, y)
-                 print(f"Iteration {_}, Loss: {lossval}")
-
-
-            self.w-=self.lr*dw
-            self.b-=self.lr*db
+    def relu(self,z):
+        return np.maximum(z,0)
+    
+    def relu_derivative(self,z):
+        return (z > 0).astype(float)
     
     def sigmoid(self,z):
         return 1/(1+np.exp(-z))
     
+    def forward(self,x):
+        self.z1=x@self.w1+self.b1
+        self.a1=self.relu(self.z1)
+        # print(self.a1)
+
+        self.z2=self.a1@self.w2+self.b2
+        self.a2=self.relu(self.z2)
+        # print(self.a2)
+
+        self.z3=self.a2@self.w3+self.b3
+        self.y_pred=self.sigmoid(self.z3)
+
+        return self.y_pred
     
-  
-    def loss(self,ycap, y):
+    def backpropagation(self,x,y):
+        m=y.shape[0]
+
+        dz3=self.y_pred-y
+
+        dw3=self.a2.T @ dz3/m
+        db3=np.mean(dz3,axis=0,keepdims=True)
+
+
+        da2=dz3 @ self.w3.T
+        dz2=da2*self.relu_derivative(self.z2)
+        dw2=self.a1.T @dz2/m
+        db2=np.mean(dz2,axis=0,keepdims=True)
+
+        da1=dz2 @ self.w2.T
+        dz1=da1*self.relu_derivative(self.z1)
+        dw1=x.T@dz1/m
+        db1=np.mean(dz1,keepdims=True,axis=0)
+
+        self.w1-=self.lr*dw1
+        self.w2-=self.lr*dw2
+        self.w3-=self.lr*dw3
+
+        self.b1-=self.lr*db1
+        self.b2-=self.lr*db2
+        self.b3-=self.lr*db3
+    def loss(self, y):
         eps = 1e-9
-        return -np.mean(self.w1 * y * np.log(ycap + eps) + self.w0 * (1 - y) * np.log(1 - ycap + eps))
-    
-    def predict(self,x,threshold=0.5):
-        z=x@self.w+self.b
-        act=self.sigmoid(z)
-        return (act >= threshold).astype(int)
-        
-
-        
-
+        return -np.mean(y*np.log(self.y_pred+eps) +
+                        (1-y)*np.log(1-self.y_pred+eps))
 
  
 data=pd.read_csv("dataset2.csv")
